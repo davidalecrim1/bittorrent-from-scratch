46d4ab0f571182187ee733e184fc4a493975f5f4
David Alecrim <dsalecrim@outlook.com>
2025-09-09 16:42:28 -0300

    chore: move connections to types

--- src/client.rs
+++ src/client.rs
@@ -7,8 +7,7 @@ use sha1::{Sha1, Digest};
 
 use crate::encoding::Decoder;
 use crate::encoding::Encoder;
-use crate::types::BencodeTypes;
-use crate::types::Peer;
+use crate::types::{BencodeTypes, Peer, PeerHandshake};
 
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpStream;
@@ -216,60 +215,4 @@ impl BitTorrent {
         Ok(response)
     }
 
-}
-
-pub struct PeerHandshake {
-    info_hash: [u8; 20], // NOT the hexadecimal string, but the actual bytes
-    peer_id: [u8; 20],
-    reserved: [u8; 8],
-}
-
-impl PeerHandshake {
-    pub fn new(info_hash: [u8; 20], peer_id: [u8; 20]) -> Self {
-        Self {
-            info_hash,
-            peer_id,
-            reserved: [0; 8],
-        }
-    }
-
-    pub fn to_bytes(&self) -> Vec<u8> {
-        let mut bytes = Vec::with_capacity(68);
-        // pstrlen (1 byte)
-        bytes.push(19);
-        // pstr (19 bytes)
-        bytes.extend_from_slice(b"BitTorrent protocol");
-        // reserved (8 bytes)
-        bytes.extend_from_slice(&self.reserved);
-        // info_hash (20 bytes)
-        bytes.extend_from_slice(&self.info_hash);
-        // peer_id (20 bytes)
-        bytes.extend_from_slice(&self.peer_id);
-
-        return bytes;
-    }
-
-    pub fn from_bytes(bytes: &[u8]) -> Result<Self> {
-        if bytes.len() != 68 {
-            return Err(anyhow!("the provided data is not a valid handshake"));
-        }
-
-        let info_hash = &bytes[19..39];
-        let peer_id = &bytes[39..59];
-        let reserved = &bytes[59..67];
-
-        Ok(Self {
-            info_hash: info_hash.try_into()?,
-            peer_id: peer_id.try_into()?,
-            reserved: reserved.try_into()?,
-        })
-    }
-}
-
-impl Debug for PeerHandshake {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let info_hash = hex::encode(self.info_hash);
-        let peer_id = hex::encode(self.peer_id);
-        write!(f, "PeerHandshake {{ info_hash: {:?}, peer_id: {:?} }}", info_hash, peer_id)
-    }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,8 @@
 use std::collections::BTreeMap;
 use std::net::IpAddr;
 use std::str::FromStr;
+use anyhow::{anyhow, Result};
+use std::fmt::Debug;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum BencodeTypes {
@@ -22,4 +24,60 @@ impl Peer {
         Self { ip: IpAddr::from_str(&ip).unwrap(), port }
     }
 
+}
+
+pub struct PeerHandshake {
+    info_hash: [u8; 20], // NOT the hexadecimal string, but the actual bytes
+    peer_id: [u8; 20],
+    reserved: [u8; 8],
+}
+
+impl PeerHandshake {
+    pub fn new(info_hash: [u8; 20], peer_id: [u8; 20]) -> Self {
+        Self {
+            info_hash,
+            peer_id,
+            reserved: [0; 8],
+        }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = Vec::with_capacity(68);
+        // pstrlen (1 byte)
+        bytes.push(19);
+        // pstr (19 bytes)
+        bytes.extend_from_slice(b"BitTorrent protocol");
+        // reserved (8 bytes)
+        bytes.extend_from_slice(&self.reserved);
+        // info_hash (20 bytes)
+        bytes.extend_from_slice(&self.info_hash);
+        // peer_id (20 bytes)
+        bytes.extend_from_slice(&self.peer_id);
+
+        return bytes;
+    }
+
+    pub fn from_bytes(bytes: &[u8]) -> Result<Self> {
+        if bytes.len() != 68 {
+            return Err(anyhow!("the provided data is not a valid handshake"));
+        }
+
+        let info_hash = &bytes[19..39];
+        let peer_id = &bytes[39..59];
+        let reserved = &bytes[59..67];
+
+        Ok(Self {
+            info_hash: info_hash.try_into()?,
+            peer_id: peer_id.try_into()?,
+            reserved: reserved.try_into()?,
+        })
+    }
+}
+
+impl Debug for PeerHandshake {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let info_hash = hex::encode(self.info_hash);
+        let peer_id = hex::encode(self.peer_id);
+        write!(f, "PeerHandshake {{ info_hash: {:?}, peer_id: {:?} }}", info_hash, peer_id)
+    }
 }
